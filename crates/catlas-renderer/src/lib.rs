a0a7298676e6010b7583a11550a898152b496a95
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2025-03-20 21:05:31 +0900

    rename: Rename renderer

--- crates/catlas-renderer/src/lib.rs
+++ crates/catlas-renderer/src/lib.rs
@@ -1,9 +1,8 @@
 pub mod error;
-pub mod render;
-pub mod render_manager;
 pub mod render_pre_tile;
 pub mod render_tile;
+pub mod renderer;
 
-pub use render::*;
 pub use render_pre_tile::*;
 pub use render_tile::*;
+pub use renderer::*;
